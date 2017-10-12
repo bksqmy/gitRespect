01760b1cd76f6d3edea06069cda1d6199eea1fed
qumingyang <bksqmy@163.com>
2017-10-12 19:49:15 +0800

    测试冲突结局

--- src/com/respect/tes/JuinTest.java
+++ src/com/respect/tes/JuinTest.java
@@ -1,11 +1,17 @@
 package com.respect.tes;
 
+import java.util.HashMap;
+import java.util.Map;
+
 
 public class JuinTest {
 	
 	
 	public static void main(String[] args) {
 		System.out.println("junit test");
+		Map<String, Object> maps = new HashMap<>();
+		
+		
 	}
 	
 	
